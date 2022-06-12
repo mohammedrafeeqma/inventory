a4c084d603528a20730833e1560d084deab1134d
mohammedrafeeqma <mohammedrafeeqma@gmail.com>
2022-06-12 16:32:46 +0530

    completed

--- frontend/src/components/removeProducts/RemoveProducts.jsx
+++ frontend/src/components/removeProducts/RemoveProducts.jsx
@@ -33,7 +33,7 @@ function RemoveProducts() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(array);
-    const res = await axios.put("http://127.0.0.1:3001/api/", array);
+    const res = await axios.put("http://rafeeqma.tk/api/", array);
     if (res) {
       window.location.reload();
     }
